97e1f9130955bc6f327642098dcde4cbb0f5a686
isadora fenner spohr <164195259+fennerspohr@users.noreply.github.com>
2025-11-01 15:44:23 -0300

    correções em createBanco.sql

--- createBanco.sql
+++ createBanco.sql
@@ -40,7 +40,7 @@ CREATE TABLE Etapa_Relacao (
     template_id INT NOT NULL,
     etapa_id INT NOT NULL,
     ordem INT NOT NULL,
-    responsavel INT,
+    responsavel INT NOT NULL,
     PRIMARY KEY (template_id, etapa_id, ordem), 
     FOREIGN KEY (template_id) REFERENCES Template(template_id),
     FOREIGN KEY (etapa_id) REFERENCES Etapas(etapa_id),
@@ -51,19 +51,22 @@ CREATE TABLE Etapa_Relacao (
 
 create table Processo (
 	processo_id int primary key auto_increment,
-    template int references Template(template_id),
-    usuario int references Usuarios(usuario_id)
+    template int NOT NULL,
+    usuario int NOT NULL,
+	FOREIGN KEY (template) REFERENCES Template(template_id),
+	FOREIGN KEY (usuario) REFERENCES Usuarios(usuario_id)
 );
 
 
 create table Processo_Etapa(
 	processo_etapa_id INT PRIMARY KEY AUTO_INCREMENT,
-    processo int references Processo(processo_id),
+    processo int not null,
     template int not null,
     etapa int not null,
     ordem int not null,
-    status_ varchar(255),
-    usuario int,
+    status_ varchar(255) not null,
+    usuario int not null,
+	FOREIGN KEY (processo) REFERENCES Processo(processo_id), 
     FOREIGN KEY (usuario) REFERENCES Usuarios(usuario_id),
 	FOREIGN KEY (template, etapa, ordem) 
     REFERENCES Etapa_Relacao (template_id, etapa_id, ordem)
@@ -72,9 +75,9 @@ create table Processo_Etapa(
 
 create table solicitacao (
 	solicitacao_id INT PRIMARY KEY AUTO_INCREMENT,
-    processo_etapa int,
-    cpf varchar(255),
-    animal int,
+    processo_etapa int not null,
+    cpf varchar(255) not null,
+    animal int not null,
     comprovante_residencia blob,
     FOREIGN KEY (animal) REFERENCES Animais (animal_id),
     FOREIGN KEY(processo_etapa) REFERENCES Processo_Etapa (processo_etapa_id)
@@ -82,24 +85,24 @@ create table solicitacao (
 
 create table visitacao (
 	visitacao_id INT PRIMARY KEY AUTO_INCREMENT,
-    data_ DATE,
-    endereco varchar(255),
-    processo_etapa int,
+    data_ DATE not null,
+    endereco varchar(255) not null,
+    processo_etapa int not null,
 	FOREIGN KEY(processo_etapa) REFERENCES Processo_Etapa (processo_etapa_id)
 );
 
 create table entrevista (
 	entrevista_id INT PRIMARY KEY AUTO_INCREMENT,
-    data_ DATE,
+    data_ DATE not null,
     observacoes varchar(255),
-    processo_etapa int,
+    processo_etapa int not null,
 	FOREIGN KEY(processo_etapa) REFERENCES Processo_Etapa (processo_etapa_id)
 );
 
 create table recusa (
 	recusa_id INT PRIMARY KEY AUTO_INCREMENT,
     justificativa varchar(255),
-    processo_etapa int,
+    processo_etapa int not null,
 	FOREIGN KEY(processo_etapa) REFERENCES Processo_Etapa (processo_etapa_id)
 );
 
